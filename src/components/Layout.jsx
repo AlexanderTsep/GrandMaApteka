05c07e01e35a95e1064b1d4505b4a6260cdbb597
AlexanderTsep <tsepaserg@ya.ru>
2023-06-07 17:34:06 +0300

    +bootstrap

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,6 +15,17 @@ export default function Layout({ initState }) {
             __html: `window.initState=${JSON.stringify(initState)}`,
           }}
         />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
+        />
         <script defer src="/app.js" />
         <script defer src="/vendor.js" />
         <title>Document</title>
