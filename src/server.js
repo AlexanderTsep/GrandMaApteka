1cf2a830afb073ae9391470a11a0a9bb0ec2ed70
AlexanderTsep <tsepaserg@ya.ru>
2023-06-09 10:54:20 +0300

    eto nepravilno, eto nelzya zabirat

--- src/server.js
+++ src/server.js
@@ -6,50 +6,44 @@ import path from 'path';
 import jsxRender from './utils/jsxRender';
 import indexRouter from './routes/indexRouter';
 import authRouter from './routes/authRouter';
-import { userInsession } from './middlewares/userMiddlewares';
-import apiAuthRouter from './routes/apiAuthRouter';
-
-require('dotenv').config();
+// import apiRouter from './routes/apiRouter';
 
 const FileStore = store(session);
 
 const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 
-app.engine('jsx', jsxRender);
-app.set('view engine', 'jsx');
 
-app.set('views', path.join(__dirname, 'components'));
+const sessionConfig = {
+  name: 'sId',
+  secret: process.env.SESSION_SECRET ?? 'test',
+  resave: false,
+  store: new FileStore(),
+  saveUninitialized: false,
+  app.engine('jsx', jsxRender);
+  app.set('view engine', 'jsx');
+  app.set('views', path.join(__dirname, 'components'));
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 12,
+    httpOnly: true,
+  },
+};
+require('dotenv').config();
 
+
+app.use(express.static('public'));
 app.use(morgan('dev'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(session(sessionConfig));
 
 app.use((req, res, next) => {
   res.locals.path = req.originalUrl;
   next();
 });
 
-app.use(
-  session({
-    name: 'sId',
-    secret: process.env.SESSION_SECRET ?? 'test',
-    resave: false,
-    store: new FileStore(),
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 12,
-      httpOnly: true,
-    },
-  }),
-);
-
-app.use(userInsession);
-
 app.use('/', indexRouter);
+// app.use('/api/auth', apiRouter);
 app.use('/auth', authRouter);
-app.use('/api/auth', apiAuthRouter);
 
 app.listen(PORT, () => console.log(`App has started on port ${PORT}`));
